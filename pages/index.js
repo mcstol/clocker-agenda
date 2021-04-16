60fbb50f26b0e14c77880497142f90c5a1cb5861
Marcelo Oliveira <santos.marcelooliveira@gmail.com>
2021-04-15 23:02:22 -0300

    feat: redirect logged user

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,31 @@
-import Link from 'next/link'
+import { useState, useEffect } from "react";
 
-import { useFormik } from 'formik'
-import * as yup from 'yup'
+import { Container, Spinner } from "@chakra-ui/react";
 
-import {
-  Container,
-  Box,
-  Input,
-  Button,
-  Text,
-  FormControl,
-  FormLabel,
-  FormHelperText,
-} from '@chakra-ui/react'
+import { Login, Agenda  } from "../components";
+import firebase from './../config/firebase'
 
-import { Logo } from '../components'
-import firebase,{ persistenseMode } from '../config/firebase'
-import { useEffect } from 'react'
-
-
-
-const validationSchema = yup.object().shape({
-  email: yup.string().email('Email inválido').required('Preenchimento obrigatório!'),
-  password: yup.string().required('Preenchimento obrigatório!'),
-})
-
-export default function Home() {
-  const {
-    values,
-    errors,
-    touched,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-    isSubmitting
-  } = useFormik({
-    onSubmit: async (values, form)=>{
-      firebase.auth().setPersistence(persistenseMode)
-      try{
-        const user = await firebase.auth().signInWithEmailAndPassword(values.email, values.password)
-        console.log(user)
-      }catch(error) {
-        console.log('ERROR:', error)
-      }
-    },
-    validationSchema,
-    initialValues: {
-      email:'',
-      username:'',
-      password:''
-    }
+export default function Home () {
+  const [auth, setAuth ] = useState({
+    loading: true,
+    user: false
   })
-  
   useEffect(()=>{
-    console.log('Sessão ativa?', firebase.auth().currentUser)
+    firebase.auth().onAuthStateChanged(user =>(
+      setAuth({
+        loading:false,
+        user
+      })
+    )
+    )  
   },[])
-
-
-
-   return (
-    <Container p={4} centerContent>
-      < Logo/>
-      <Box p={4} mt={8}>
-        <Text>Crie sua agenda compartilhada</Text>
-      </Box>
-      <Box>
-        <FormControl id="email" p={4} isRequired>
-          <FormLabel>Email</FormLabel>
-          <Input size="lg" type="email" value={values.email} onChange={handleChange} onBlur={handleBlur}/>
-          {touched.email && <FormHelperText textColor="#e74c3c">{errors.email}</FormHelperText>}
-        </FormControl>
-
-        <FormControl id= "password" p={4} isRequired>
-          <FormLabel>Senha</FormLabel>
-          <Input size="lg" type="password" value={values.password} onChange={handleChange} onBlur={handleBlur}/>
-          {touched.password && <FormHelperText textColor="#e74c3c">{errors.password}</FormHelperText>}
-        </FormControl>
-
-        <Box p={4}>
-          <Button colorScheme="blue" width="100%" onClick={handleSubmit} isLoading={isSubmitting}>Entrar</Button>
-        </Box>
-
-      </Box>
-      <Link href="/signup">Ainda não tem uma conta? Cadastre-se!</Link>
+  if(auth.loading) {
+    return(
+    <Container p={4} centerContent> 
+      <Spinner/>
     </Container>
-  )
+    )
+  }
+  // const authenticatedUser = firebase.auth().currentUser
+  return auth.user ? <Agenda/> : <Login/>
 }
